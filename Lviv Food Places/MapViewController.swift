82caea42ed90cfa673012e94481e67050589fcd5
Ihor <56516292+degster55@users.noreply.github.com>
2022-01-30 01:17:00 +0200

    added new file 'MapViewController', work with storyboard, added images for buttons

---
+++ Lviv Food Places/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  Lviv Food Places
+//
+//  Created by Ігор Ляхович on 30.01.2022.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+}
