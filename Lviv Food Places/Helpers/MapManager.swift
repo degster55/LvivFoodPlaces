63a8e9dc979605b9f1e0555c8b11fcce862cddc4
Ihor <56516292+degster55@users.noreply.github.com>
2022-01-31 00:32:05 +0200

    Refactoring project, grouped files

---
+++ Lviv Food Places/Helpers/MapManager.swift
@@ -0,0 +1,203 @@
+//
+//  MapManager.swift
+//  Lviv Food Places
+//
+//  Created by Ігор Ляхович on 30.01.2022.
+//
+
+import UIKit
+import MapKit
+
+class MapManager {
+    let locationManager = CLLocationManager()
+    
+    private var placeCoordinate: CLLocationCoordinate2D?
+    private let regionInMeters = 1000.00
+    private var directionsArray: [MKDirections] = []
+    
+    
+    // Маркер закладу
+    func setupPlacemark(place: Place, mapView: MKMapView) {
+        
+        guard let location = place.location else { return }
+        
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(location) { (placemarks, error) in
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let placemarks = placemarks else { return }
+            
+            let placemark = placemarks.first
+            
+            let annotation = MKPointAnnotation()
+            annotation.title = place.name
+            annotation.subtitle = place.type
+            
+            guard let placemarkLocation = placemark?.location else { return }
+            
+            annotation.coordinate = placemarkLocation.coordinate
+            self.placeCoordinate = placemarkLocation.coordinate
+            
+            mapView.showAnnotations([annotation], animated: true)
+            mapView.selectAnnotation(annotation, animated: true)
+        }
+    }
+    
+    // Перевірка доступності сервісів геолокації
+    func checkLocationServices(mapView: MKMapView, segueIdentifier: String, closure: () -> ()) {
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            checkLocationAuthorization(mapView: mapView, segueIdentifier: segueIdentifier)
+            closure()
+        } else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.showAlert(title: "Location Services are Disabled",
+                               message: "To enable it go: Setting -> Privacy -> Location Services and turn On")
+            }
+        }
+    }
+    
+    // Перевірка авторизації додатку для використання сервісів геолокації
+    func checkLocationAuthorization(mapView: MKMapView, segueIdentifier: String) {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            if segueIdentifier == "getAddress" { showUserLocation(mapView: mapView)}
+            break
+        case .denied:
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.showAlert(title: "Your Location is not Available",
+                               message: "To enable your location tracking: Setting -> Lviv Food Places -> Location")
+            }
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            print("New case is available")
+        }
+    }
+    
+    // Фокус карти на місцезнаходження користувача
+    func showUserLocation(mapView: MKMapView) {
+        
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location,
+                                            latitudinalMeters: regionInMeters,
+                                            longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    // Будуємо маршрут від місцезнаходження користувача до закладу
+    func getDirections(for mapView: MKMapView, previousLocation: (CLLocation) ->()) {
+        
+        guard let location = locationManager.location?.coordinate else {
+            showAlert(title: "Error", message: "Current location is not found")
+            return
+        }
+        
+        locationManager.startUpdatingLocation()
+        previousLocation(CLLocation(latitude: location.latitude, longitude: location.longitude))
+        
+        guard let request = createDirectionsRequest(from: location) else {
+            showAlert(title: "Error", message: "Destination is not found")
+            return
+        }
+        
+        let directions = MKDirections(request: request)
+        
+        resetMapView(withNew: directions, mapView: mapView)
+        
+        directions.calculate { (response, error) in
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let response = response else {
+                self.showAlert(title: "Error", message: "Directions is not available")
+                return
+            }
+            
+            for route in response.routes {
+                mapView.addOverlay(route.polyline)
+                mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+                
+                let distance = String(format: "%.1f", route.distance / 1000)
+                let timeInterval = route.expectedTravelTime
+                
+                print("Відстань до місця: \(distance) км.")
+                print("Відстань в дорозі: \(timeInterval) сек.")
+            }
+        }
+    }
+    
+    // Налаштування запиту для розрахування маршруту
+    func createDirectionsRequest(from coordinate: CLLocationCoordinate2D) -> MKDirections.Request? {
+        
+        guard let destinationCoordinate = placeCoordinate else { return nil }
+        let startingLocation = MKPlacemark(coordinate: coordinate)
+        let destination = MKPlacemark(coordinate: destinationCoordinate)
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: startingLocation)
+        request.destination = MKMapItem(placemark: destination)
+        request.transportType = .walking
+        request.requestsAlternateRoutes = true
+        
+        return request
+    }
+    
+    // Міняємо відображувану зону області карти відповідно з переміщенням користувача
+    func startTrackingUserLocation(for mapView: MKMapView, and location: CLLocation?, closure: (_ currentLocation: CLLocation) -> ()) {
+        
+        guard let location = location else { return }
+        let center = getCenterLocation(for: mapView)
+        guard center.distance(from: location) > 50 else { return }
+        
+        closure(center)
+    }
+    
+    // Скидання всіх раніше побудованих маршрутів перед побудовою нового
+    func resetMapView(withNew directions: MKDirections, mapView: MKMapView) {
+        
+        mapView.removeOverlays(mapView.overlays)
+        directionsArray.append(directions)
+        let _ = directionsArray.map {$0.cancel() }
+        directionsArray.removeAll()
+    }
+    
+    // Визначення центру області карти, що відображається
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+    
+    func showAlert(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        
+        let alertWindow = UIWindow(frame: UIScreen.main.bounds)
+        alertWindow.rootViewController = UIViewController()
+        alertWindow.windowLevel = UIWindow.Level.alert + 1
+        alertWindow.makeKeyAndVisible()
+        alertWindow.rootViewController?.present(alert, animated: true)
+    }
+}
+
